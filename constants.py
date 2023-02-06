86185212563ba902ca6d94ece2ce7424eaf7de33
kiranhegde <kiranh@sensehawk.com>
2023-02-06 12:44:21 +0530

    minor fix

--- constants.py
+++ constants.py
@@ -10,7 +10,7 @@ SCM_INFERENCE_URL = "https://scm-inference.sensehawk.com"
 SID_URL = "https://sid.sensehawk.com/detect-solar-issues"
 CLIP_FUNCTION_URL = "https://bt0xyuwjxi.execute-api.us-west-2.amazonaws.com/default/clipRaster"
 
-# Staging env
+# # Staging env
 # CORE_URL = "https://core-stage-server.sensehawk.com"
 # MAP_SERVER_URL = "https://mapserver.sensehawk.com/"
 # TERRA_URL = "https://terra-stage-server.sensehawk.com"
@@ -18,3 +18,4 @@ CLIP_FUNCTION_URL = "https://bt0xyuwjxi.execute-api.us-west-2.amazonaws.com/defa
 # SCM_TRAIN_URL = "https://scm-train-stage.sensehawk.com"
 # SCM_INFERENCE_URL = "https://scm-inference-stage.sensehawk.com"
 # CLIP_FUNCTION_URL = "https://848rwfqtw0.execute-api.us-west-2.amazonaws.com/default/clipRaster"
+# SID_URL = "https://sid-stage.sensehawk.com/detect-solar-issues"
